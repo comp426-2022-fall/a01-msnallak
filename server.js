2fec415fd7d97d6b9bf30463b105f63d03170a0b
Your Name <msnallak>
2022-09-01 12:03:25 -0400

    fixes

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
 const minimist = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-process.argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-});
 
 const args = require('minimist')(process.argv.slice(2));
 const port = args.port || 3000;
@@ -27,8 +24,8 @@ fs.readFile(fileName, 'utf8', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 	const server = http.createServer((req, res)=>{
-		res.setHeader('Content-Type', 'text/html')
 		res.statusCode = 200
+		res.setHeader('Content-Type', 'text/html')
 		res.end(data);
 	})
 	server.listen(port, () => {
